3d2711feda255bf6d137bce954b0b6190289c911
unknown <nishavkumar07@gmail.com>
2026-02-05 19:57:32 +0530

    making new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import './App.css'
 
-function App() {
-
-
+function Navbar() {
   return (
     <>
+    <div id="navbar">
+      <div id="logo"><h1>React</h1></div>
+      <div id='menu'>
+        <a href="#">menu</a>
+        <a href="#">bro</a>
+      </div>
+    </div>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+    <Navbar/>
       <h1>hello world</h1>
       <button id='b'>Button</button>
     </>
   )
 }
+
+
 
 export default App
