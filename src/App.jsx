af62170431f98b55dc6121094e87de5fa0de2bb9
unknown <nishavkumar07@gmail.com>
2026-02-06 20:17:00 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Thumbnail from './thumbnail'
 
 
 
 function App() {
   return (
     <>
+    <thumbnail/>
     </>
   )
 }
---
+++ src/thumbnail.jsx
@@ -0,0 +1,12 @@
+
+import "thumbnail.css"
+function Thumbnail(){
+    return (
+        <div id="thumbnail">
+
+        </div>
+    )
+}
+
+
+export default Thumbnail;
