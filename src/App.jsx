51324576241993f37aebc764178cbaffbfedf6cf
unknown <nishavkumar07@gmail.com>
2026-02-06 20:07:49 +0530

    recreating yt thumbnail

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css'
-import Navbar from './navbar';
+
 
 
 function App() {
   return (
     <>
-    <Navbar/>
-      <h1>hello world</h1>
-      <button id='b'>Button</button>
     </>
   )
 }
---
+++ src/title.jsx
@@ -0,0 +1,15 @@
+
+function Title(){
+    let chennel = "NISHAV CODE";
+    let views = "10M";
+    let time = "23 hours ago";
+    return (
+        <div>
+            <h1>How to beacme a dev</h1>
+            <p id="Cname">{chennel}</p>
+            <p>{views} . {time}</p>
+        </div>
+    )
+}
+
+export default Title;
