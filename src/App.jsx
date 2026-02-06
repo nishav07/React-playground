c67d9d91ad26e4f5146f8a0c87c8796a861860c3
unknown <nishavkumar07@gmail.com>
2026-02-06 19:05:51 +0530

    understanding jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
 import './App.css'
+import Navbar from './navbar';
 
-function Navbar() {
-  return (
-    <>
-    <div id="navbar">
-      <div id="logo"><h1>React</h1></div>
-      <div id='menu'>
-        <a href="#">menu</a>
-        <a href="#">bro</a>
-      </div>
-    </div>
-    </>
-  )
-}
 
 function App() {
   return (
---
+++ src/navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <>
+    <div id="navbar">
+      <div id="logo"><h1>React</h1></div>
+      <div id='menu'>
+        <a href="#">menu</a>
+        <a href="#">bro</a>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
+
