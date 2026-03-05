dc5099836ddf0e70a5ea9ea91539a0450bd341da
unknown <nishavkumar07@gmail.com>
2026-03-05 14:26:06 +0530

    trying crud in state var

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+
 
 
 export default function TodoList () {
-    let [todo,setTodo] = useState(["sample task"]);
+    let [todo,setTodo] = useState([{task:"sample task", id:uuidv4()}]);
     let [newTodo,setNewTodo] = useState([""])
-    console.log(`todo before rendered ${todo}`);
+    // console.log(`todo before rendered ${todo.id}`);
 
     let updateTodo = (e) => {
-        console.log(e.target.value);
+        // console.log(e.target.value);
         setNewTodo(e.target.value);
     }
 
@@ -17,10 +20,15 @@ export default function TodoList () {
         console.log("Naya todo hai ye",newTodo);
         console.log("new task add krna haii");
         setTodo((prevTodo) => {
-            return [...prevTodo,newTodo];
+            return [...prevTodo,{task:newTodo,id:uuidv4()}];
         })
         setNewTodo("");
     }
+
+    let deleteFromTodo = (id) => {
+        console.log("trying to delete this shitt",id);
+    }
+
     return (<div>
         <input type="text" placeholder="enter your work" value={newTodo} onChange={updateTodo} />
 <button onClick={addNewTask}>Add</button>
@@ -30,7 +38,16 @@ export default function TodoList () {
 
 <ul>
     {todo.map((todos) => (
-        <li>{todos}</li>
+        <li key={todos.id}>
+            <span>{todos.task}</span>
+            &nbsp;
+            &nbsp;
+            <button onClick={() => {
+                deleteFromTodo(todos.id)
+            }}>delete</button>
+        </li>
+
+        
     ))}
 </ul>
     </div>
