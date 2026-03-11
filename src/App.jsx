10ccccdcdfc4b81cef900a8661be0b643ce100a9
unknown <nishavkumar07@gmail.com>
2026-03-11 17:14:33 +0530

    lifting satate up

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import Card from './tempFile/Card.jsx';
 import Msgbox from './tempFile/Msgbox.jsx';
 import Video from './Video.jsx';
 import TodoList from './TodoList.jsx';
+import LotteryGame from './LotteryGame.jsx';
 
 function App() {
+
+  
   return (
    <>
-    <TodoList/>
+   <LotteryGame/>
    </>
   )
 }
---
+++ src/LotteryGame.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+
+
+export default function LotteryGame(){
+
+    let [Num,setNum] = useState(
+        [Math.floor(Math.random() * 9) + 1,
+        Math.floor(Math.random() * 9) + 1,
+        Math.floor(Math.random() * 9) + 1]
+    );
+    
+
+    let isWinning = false;
+
+    function generateNew(){
+        let n1 = Math.floor(Math.random() * 9) + 1;
+        let n2 = Math.floor(Math.random() * 9) + 1;
+        let n3 = Math.floor(Math.random() * 9) + 1;
+
+        setNum([n1,n2,n3 ])
+
+        if(n1 + n2 + n3 == 15){
+            alert("you win the lottery")
+          
+        } else {
+            console.log("try agian")
+        }
+        
+    }
+
+
+    return(<>
+    <h1>lottery game</h1>
+    <h2>Lottery number</h2>
+    <p>{Num[0]},{Num[1]},{Num[2]}</p>
+    <button onClick={() => {
+        generateNew()
+    }}>Generate New</button>
+    </>)
+}
